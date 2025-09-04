b0af6b2c5767907c0721c4b35b69093a2161fcfb
unknown <ohadlesh@gmail.com>
2025-09-04 17:06:57 +0300

    wrong bug targeted earlier. fix different issue with image optimization inside docker

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,   // keep strict mode enabled
+  images: {
+    unoptimized: true,     // disable built-in image optimization to fix .wasm build error
+  },
+};
+
+module.exports = nextConfig;
